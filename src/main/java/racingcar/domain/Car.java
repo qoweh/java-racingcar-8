6a77bb91c11e07e6b0faa180b10326a1030a7698
MWK <sunnybikers@naver.com>
2025-10-23 15:49:01 +0900

    feat: Output 로직 처리
    * OutputView 만들어서 GameController에서 처리
    * OutputView에서는 Car 리스트를 입력받아 "각 회차 별 결과"를 출력할 수 있고, name 리스트를 입력받아 "최종 결과"를 출력할 수 있다.

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+public class Car {
+    private String name;
+    private int score;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+}
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,44 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.domain.Car;
+
 public class OutputView {
+    private static final String OUTPUT_STATUS_MESSAGE = "실행 결과";
+    private static final String OUTPUT_SCORE_LOG = "-";
+    private static final String OUTPUT_STATUS_FORMAT = " : ";
+    private static final String OUTPUT_RESULT_MESSAGE = "최종 우승자 : ";
+
+    public static void presentStatus(List<Car> cars) {
+        System.out.println(OUTPUT_STATUS_MESSAGE);
+
+        for (Car car : cars) {
+            eachCarStatus(car);
+        }
+
+        System.out.println();
+    }
+
+    private static void eachCarStatus(Car car) {
+        String name = car.getName();
+
+        int score = car.getScore();
+        String scoreLog = OUTPUT_SCORE_LOG.repeat(score);
+
+        System.out.println(name + OUTPUT_STATUS_FORMAT + scoreLog);
+    }
+
+    public static void presentResult(List<String> names) {
+        String result = joinString(names);
+
+        System.out.println(OUTPUT_RESULT_MESSAGE + result);
+    }
+
+    private static String joinString(List<String> names) {
+        if (names.size() == 1) {
+            return names.get(0);
+        }
+
+        return String.join(",", names);
+    }
 }
