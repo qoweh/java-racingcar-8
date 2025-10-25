5ea06d15e299e1e5aed493a742c439425b2d05c8
MINWOO KIM <sunnybikers@naver.com>
2025-10-25 18:53:06 +0900

    refactor: 가독성 향상을 위해 검증 메서드 분리
    * CountValidator의 검증 메서드들을 private 메서드들로 분리

--- src/main/java/racingcar/service/validator/CountValidator.java
+++ src/main/java/racingcar/service/validator/CountValidator.java
@@ -6,11 +6,19 @@ public class CountValidator extends InputValidator {
     private static final String NUMBER_TYPE = "(\\s)*-?\\d+(\\s)*";
 
     public void isValidNumberString(String input) {
+        validateNumber(input);
+        validatePositiveNumber(input);
+    }
+
+    private void validateNumber(String input) {
         if (!input.matches(NUMBER_TYPE)) {
             throw new IllegalArgumentException(
                     ValidatorMessage.INVALID_NUMBER.with(input)
             );
         }
+    }
+
+    private void validatePositiveNumber(String input) {
         if (input.contains("-")) {
             throw new IllegalArgumentException(
                     ValidatorMessage.NEGATIVE_NUMBER.with(input)
@@ -21,15 +29,19 @@ public class CountValidator extends InputValidator {
     public int isValidSizeAndGet(String input) {
         try {
             int number = Integer.parseInt(input);
-            if (number == 0) {
-                throw new IllegalArgumentException(
-                        ValidatorMessage.ZERO_NUMBER.with(String.valueOf(input))
-                );
-            }
+            validateGreaterThanZero(input, number);
             return number;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(
                     ValidatorMessage.TOO_BIG.with(String.valueOf(input))
+            );
+        }
+    }
+
+    private void validateGreaterThanZero(String input, int number) {
+        if (number == 0) {
+            throw new IllegalArgumentException(
+                    ValidatorMessage.ZERO_NUMBER.with(String.valueOf(input))
             );
         }
     }
